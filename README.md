90eabc07b4828f8f80679c6e5fd30da73daf500a
zhengyy <790436992@qq.com>
2018-12-20 16:32:04 +0800

    001

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 通用框架搭建
 
 springboot
-使用shiro来控制前后端分离权限，请求中header添加token
+跨域配置
+shiro控制前后端分离权限
+多数据源配置
+dubbo配置
 
--- manage-system/src/main/java/com/zyy/PlatformApplication.java
+++ manage-system/src/main/java/com/zyy/PlatformApplication.java
@@ -7,10 +7,12 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @Import({DynamicDataSourceConfig.class})
+@ImportResource(value = {"classpath:dubbo-manage-system.xml"})
 public class PlatformApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
